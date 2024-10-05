37725fa7f874e7172bf2995115f77bb572a6307b
Максим Гайдидей <m.gaididei@apeclix.com>
2024-10-05 15:11:37 +0400

    fish

---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,10 @@
+function fish_prompt
+    set -l last_status $status
+
+    set -l stat
+    if test $last_status -ne 0
+        set stat (set_color red)"[$last_status]"(set_color normal)
+    end
+
+    echo -n (set_color green)(whoami)(set_color normal) in (set_color green)(prompt_pwd)(set_color normal) $stat '> '
+end
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Installing alacritty..."
-
-rm -f ~/.config/alacritty
-ln -s $(pwd)/alacritty ~/.config/alacritty
