522cc6b51e70383ca5d0a835eb49a794369928c5
Firebain <firebain@yandex.ru>
2026-02-15 13:15:56 +0400

    Return back

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -18,10 +18,8 @@ return {
     end, { desc = 'Find Files' })
 
     vim.keymap.set('n', '<leader><space>', function()
-      Snacks.picker.buffers {
-        current = false,
-      }
-    end, { desc = 'Find Buffers' })
+      Snacks.picker.recent()
+    end, { desc = 'Find Recent Files' })
 
     vim.keymap.set('n', '<leader>fg', function()
       Snacks.picker.grep()
